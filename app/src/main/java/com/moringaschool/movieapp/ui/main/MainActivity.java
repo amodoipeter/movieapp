526b3e0820d195515f3deb27da90b749d927f873
amodoipeter <amodoipeter@gmail.com>
2020-12-09 22:55:04 +0300

    Add tab page adapter

--- app/src/main/java/com/moringaschool/movieapp/ui/main/MainActivity.java
+++ app/src/main/java/com/moringaschool/movieapp/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moringaschool.movieapp;
+package com.moringaschool.movieapp.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -6,6 +6,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.moringaschool.movieapp.R;
 
 /**
  * @author moringa
